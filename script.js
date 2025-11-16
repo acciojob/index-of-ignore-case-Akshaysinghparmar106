44080d97025c86df06d42bc6cedc7870e40b34e4
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-11-16 11:36:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function indexOfIgnoreCase(s1, s2) {
   return str.toLowerCase().indexOf(subStr.toLowerCase());
 }
 
-	console.log(indexOfIgnoreCase("Hello World", "world")); // 6
-console.log(indexOfIgnoreCase("apple", "Ple"));         // 2
-console.log(indexOfIgnoreCase("test", "aaa"));
+	//console.log(indexOfIgnoreCase("Hello World", "world")); // 6
+//console.log(indexOfIgnoreCase("apple", "Ple"));         // 2
+//console.log(indexOfIgnoreCase("test", "aaa"));
 
 	
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
+const s1 = prompt(Hello World", "world);
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
