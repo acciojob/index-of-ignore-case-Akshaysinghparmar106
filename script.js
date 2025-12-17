9b5e6d24512f4e17726c068a784f17912fa0e5e3
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-12-17 22:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	function indexOfIgnoreCase(str, subStr) {
-    // If subStr is empty, return 0 (same behavior as normal indexOf)
-    if (subStr.length === 0) return 0;
-
-    // Convert both strings to lowercase for case-insensitive comparison
-    const lowerStr = str.toLowerCase();
-    const lowerSub = subStr.toLowerCase();
-
-    // Use indexOf to find the substring
-    return lowerStr.indexOf(lowerSub);
-}
-
+	if(s1.length < s2.length){
+		return -1;
+	}
+	return s1.toLowerCase().indexof(s2.toLowerCase());
+	console.log(indexOfIgnoreCase("apple", "ple"));
+	console.log(indexOfIgnoreCase("Hello World", "world"));
+	console.log(indexOfIgnoreCase("test," "aaa"));
+	
