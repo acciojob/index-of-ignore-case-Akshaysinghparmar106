523a68faa6c5ae761722a0fd33903994b6a94029
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-12-17 22:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	if(s1.length < s2.length){
-		return -1;
-	}
-	return s1.toLowerCase().indexof(s2.toLowerCase());
-	//console.log(indexOfIgnoreCase("apple", "ple"));
-	console.log(indexOfIgnoreCase("Hello World", "world"));
-	//console.log(indexOfIgnoreCase("test," "aaa"));
+	function indexOfIgnoreCase(str, subStr) {
+  const main = str.toLowerCase();
+  const search = subStr.toLowerCase();
+
+  for (let i = 0; i <= main.length - search.length; i++) {
+    let match = true;
+
+    for (let j = 0; j < search.length; j++) {
+      if (main[i + j] !== search[j]) {
+        match = false;
+        break;
+      }
+    }
+
+    if (match) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+(indexOfIgnoreCase("test," "aaa"));
 	
